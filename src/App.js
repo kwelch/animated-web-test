3571f7b71fd24235047d8da54854e5de9a8b1588
Kyle Welch <kwelch0626@gmail.com>
2017-04-11 21:47:49 -0500

    added animated grow and rotate

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
 import React from 'react';
+import Animated from 'animated/lib/targets/react-dom';
 import './base.css'; // eslint-disable-line import/no-unassigned-import
 
-const App = () => {
-  return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
-  );
-};
+const Div = Animated.createAnimatedComponent('div');
+
+class App extends React.Component {
+  state = {
+    rotation: new Animated.Value(10),
+  };
+
+  componentWillMount() {
+    this.interpolate = this.state.rotation.interpolate({
+      inputRange: [-800, 0, 800],
+      outputRange: ['-360deg', '0deg', '360deg'],
+    });
+  }
+
+  _handleClick = () => {
+    Animated.decay(this.state.rotation, { velocity: 1 }).start();
+  };
+
+  render() {
+    return (
+      <Div
+        style={{
+          height: this.state.rotation,
+          width: this.state.rotation,
+          backgroundColor: '#757575',
+          transform: [
+            {
+              rotate: this.interpolate,
+            },
+          ],
+        }}
+        onClick={this._handleClick}
+      />
+    );
+  }
+}
 
 export default App;
